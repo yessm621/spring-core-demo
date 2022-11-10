c9192d1ac583e540757cda46ccaab2ba78cc86a6
yessm621 <yessm621@gmail.com>
2022-11-10 10:54:45 +0900

    AppConfig 리팩토링

--- src/main/java/com/study/core/AppConfig.java
+++ src/main/java/com/study/core/AppConfig.java
@@ -1,18 +1,33 @@
 package com.study.core;
 
+import com.study.core.discount.DiscountPolicy;
 import com.study.core.discount.FixDiscountPolicy;
+import com.study.core.member.MemberRepository;
 import com.study.core.member.MemberService;
 import com.study.core.member.MemberServiceImpl;
 import com.study.core.member.MemoryMemberRepository;
 import com.study.core.order.OrderService;
 import com.study.core.order.OrderServiceImpl;
 
+/**
+ * 리팩토링을 통해 얻은 점
+ * 1. 역할과 구현클래스가 명확해짐.
+ * 2. 중복을 제거하여 변경 시 한 부분만 변경하면 됨.
+ */
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
