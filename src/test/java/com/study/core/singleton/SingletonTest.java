1dad56535199e121076c093d1306e172d0b45c35
yessm621 <yessm621@gmail.com>
2022-11-15 14:19:18 +0900

    싱글톤 적용 전

---
+++ src/test/java/com/study/core/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package com.study.core.singleton;
+
+import com.study.core.AppConfig;
+import com.study.core.member.Grade;
+import com.study.core.member.Member;
+import com.study.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotEqualTo(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
